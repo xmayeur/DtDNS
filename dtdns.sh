0546dfc4848db8c15d7b2790c3286122ccb73325
Xavier Mayeur <xavier@mayeur.be>
2018-03-08 22:55:03 +0100

    Use a vault service to retrieve host & password

--- dtdns.sh
+++ dtdns.sh
@@ -1,4 +1,3 @@
-cd ~
-docker run -ti --name dtdns --dns 192.168.0.4 -v $(pwd):/conf/ -v /var/log:/var/log/ xmayeur/dtdns
-# docker stop dtdns &&  docker rm dtdns
+docker run -ti --name dtdns -v /root/:/conf/ -v /var/log:/var/log/ xmayeur/dtdns
+
 
